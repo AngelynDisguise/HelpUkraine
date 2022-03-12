b0426cb348c2ff1e76a2fd09b623064627ccbc89
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-03-12 18:50:52 -0500

    Fixed Routing

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="conflict-context">
+        <p>Here's what's going on...</p>
+      </div>
+      <div className="call-for-action">
+        <p>Here's what you can do...</p>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <div>About</div>
+    <nav id="">
+            <ul className="nav-links">
+                <Link to='/Home'>
+                <button className="">Home</button>
+                </Link>
+                <Link to='/FindFamily'>
+                <button className="">Find Family</button>
+                </Link>
+                <Link to='/FindHost'>
+                <button className="">Find Host</button>
+                </Link>
+                <Link to='/HelpResources'>
+                <button className="">Help + Resources</button>
+                </Link>
+                <Link to='/About'>
+                <button className="">About</button>
+                </Link>
+            </ul>
+        </nav>
   )
 }
 
