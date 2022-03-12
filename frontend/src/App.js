0044b5c2d045e5e27eb2a6c5a3d4374a6f20bb46
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-03-12 17:53:39 -0500

    Set up basic landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
-import {Routes, Route,  BrowserRouter as Router } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 
-import {About, Home, BeHost, FindHost, HelpResources, FindFamily}  from './Components'
-/* import Nav from './components/Nav'; */
-import Home from './Components/Home';
-import FindHost from './Components/FindHost';
-import HelpResources from './Components/BeHost';
-import About from './Components/About';
+// import Nav from './Components/Nav';
+// import Home from './Components/Home';
+// import FindFamily from './Components/FindFamily';
+// import FindHost from './Components/FindHost';
+// import HelpResources from './Components/BeHost';
+// import About from './Components/About';
 
 function App() {
   return (
-    <div>App</div>
+    <div className="wrapper">
+      <h1>Mission: Help Ukraine Refugees</h1>
+      {/* <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/FindFamily" element={<FindFamily />} />
+        <Route path="/FindHost" element={<FindHost />} />
+        <Route path="/HelpResources" element={<HelpResources />} />
+        <Route path="/About" element={<About />} />
+      </Routes> */}
+      <div className="conflict-context">
+        <p>Here's what's going on...</p>
+      </div>
+      <div className="call-for-action">
+        <p>Here's what you can do...</p>
+      </div>
+      <div className="find-family-card">
+        <h1>Families seeking refuge</h1>
+        <ul>
+          <li>Family name:</li>
+          {/* Vague location example:  Lithuania*/}
+          <li>Location: </li>
+          <li>Status: </li>
+          {/* List of names */}
+          <li>Searching for:</li>
+        </ul>
+        <h3>Are you a family member or want to provide help?</h3>
+        <button className="contact-button">Contact</button>
+      </div>
+    </div>
   );
 }
 
